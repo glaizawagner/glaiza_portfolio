0fe4f2d48006256dd3eae2531c6a1315e2d2a3ed
Glaiza Wagner <glaizawagner@gmail.com>
2020-02-15 15:49:52 -0500

    partial

---
+++ src/Components/About/About.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+
+const About = () => {
+    
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    })
+
+    return(
+        <section className="About">
+        </section>
+    )
+}
+export default About;
